5a1bf047cb45c1bb3c08c9a67d5ee8381097aee8
ryu <ryooooooo1@gmail.com>
2018-09-04 19:17:31 +0900

    adjust style of the chouseisan

--- script.js
+++ script.js
@@ -11,6 +11,14 @@
     b = e[1].style;
 
   var s = $.createElement('button');
+  s.onmouseover = function() {
+    s.style.backgroundColor = 'transparent';
+  };
+
+  // adjust style of the chouseisan
+  s.onmouseleave = function() {
+    s.style.backgroundColor = '#EEEEEE';
+  };
   s.onclick = function() {
     a.display = 'none';
     b.display = 'table';
@@ -18,8 +26,15 @@
     a = b;
     b = t;
   };
+  s.style.fontSize = '13px';
+  s.style.backgroundColor = '#EEEEEE';
+  s.style.border = '1px solid #CCCCCC';
+  s.style.color = '#616161';
+  s.style.borderRadius = '5px';
+  s.style.outline = 'none';
   s.appendChild($.createTextNode('行と列を切り替え'));
 
+  // select the first element after .event-choice-table-header-div.
   var h = $.querySelector('.event-choice-table-header-div *');
   h.appendChild($.createTextNode(' '));
   h.appendChild(s);
