dad61be30695262e6f8a628cee359fc28fc1f1ab
ryu <ryooooooo1@gmail.com>
2018-09-04 18:09:35 +0900

    add "行と列を切り替え" button

--- popup/script.js
+++ popup/script.js
@@ -1,44 +1,57 @@
-(function () {
-    var days = ["日", "月", "火", "水", "木", "金", "土"];
-    var now = new Date();
-    function getDateStr(date) {
-        var month = date.getMonth() + 1;
-        var day = date.getDate();
-        return date.getFullYear() + "-" + (month >= 10 ? month : ("0" + month)) + "-" + (day >= 10 ? day : ("0" + day));
-    }
+(function($) {
+  var days = ['日', '月', '火', '水', '木', '金', '土'];
+  var now = new Date();
+  function getDateStr(date) {
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    return (
+      date.getFullYear() +
+      '-' +
+      (month >= 10 ? month : '0' + month) +
+      '-' +
+      (day >= 10 ? day : '0' + day)
+    );
+  }
 
-    document.addEventListener("DOMContentLoaded", function () {
-        var main = document.forms.main;
-        var from = main.from;
-        var to = main.to;
-        from.value = getDateStr(now);
-        to.value = getDateStr(new Date(+now + 86400000));
+  $.addEventListener('DOMContentLoaded', function() {
+    var main = $.forms.main;
+    var from = main.from;
+    var to = main.to;
+    from.value = getDateStr(now);
+    to.value = getDateStr(new Date(+now + 86400000));
 
-        main.addEventListener("submit", function (event) {
-            var s = from.value;
-            var e = to.value;
-            var text = main.text.value;
-            a(s, e, text);
-            event.preventDefault();
-        });
+    main.addEventListener('submit', function(event) {
+      var s = from.value;
+      var e = to.value;
+      var text = main.text.value;
+      a(s, e, text);
+      event.preventDefault();
     });
+  });
 
-    function a(from, to, text) {
-        var choseiText = [];
-        var repeat = text.split("\n");
-        var fromDate = Date.parse(from);
-        var toDate = Date.parse(to);
-        for (var i = fromDate; i <= toDate; i += 86400000) {
-            var date = new Date(i);
-            var datestr = (date.getMonth() + 1) + "/" + date.getDate() + "(" + days[date.getDay()] + ") ";
-            for (var j = 0; j < repeat.length; j++) {
-                var p = datestr + repeat[j];
-                choseiText.push(p);
-            }
-        }
-        var s = choseiText.join("\n");
-        chrome.tabs.query({ currentWindow: true, active: true }, function (tab) {
-            chrome.tabs.sendMessage(tab[0].id, { text: s });
-        })
+  function a(from, to, text) {
+    var choseiText = [];
+    var repeat = text.split('\n');
+    var fromDate = Date.parse(from);
+    var toDate = Date.parse(to);
+    for (var i = fromDate; i <= toDate; i += 86400000) {
+      var date = new Date(i);
+      var datestr =
+        date.getMonth() +
+        1 +
+        '/' +
+        date.getDate() +
+        '(' +
+        days[date.getDay()] +
+        ') ';
+      for (var j = 0; j < repeat.length; j++) {
+        var p = datestr + repeat[j];
+        choseiText.push(p);
+      }
     }
-})();
+    var s = choseiText.join('\n');
+    chrome.tabs.query({ currentWindow: true, active: true }, function(tab) {
+      chrome.tabs.sendMessage(tab[0].id, { text: s });
+    });
+  }
+})(document);
--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-(function () {
-	chrome.runtime.onMessage.addListener(function (msg, sender) {
-		document.getElementById("kouho").value = msg.text;
-	})
-})();
+(function($) {
+  chrome.runtime.onMessage.addListener(function(msg, sender) {
+    $.getElementById('kouho').value = msg.text;
+  });
+})(document);
+
+// author: https://gist.github.com/yuru4c/0069ce5724db34c9085ffe2d51b23611
+(function($) {
+  var e = $.getElementsByClassName('schedule');
+  var a = e[0].style,
+    b = e[1].style;
+
+  var s = $.createElement('button');
+  s.onclick = function() {
+    a.display = 'none';
+    b.display = 'table';
+    var t = a;
+    a = b;
+    b = t;
+  };
+  s.appendChild($.createTextNode('行と列を切り替え'));
+
+  var h = $.querySelector('.event-choice-table-header-div *');
+  h.appendChild($.createTextNode(' '));
+  h.appendChild(s);
+})(document);
